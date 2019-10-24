d02299b7ba67cb80801b9da3ba862a65830adfa7
Johan Vandegriff <johan@vandymail.com>
2019-10-24 18:17:04 -0400

    added turn left intent

--- __init__.py
+++ __init__.py
@@ -5,10 +5,12 @@ class Aimar(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('aimar.intent')
+    @intent_file_handler('left.intent')
     def handle_aimar(self, message):
-        self.speak_dialog('aimar')
+        self.speak_dialog('left')
 
+def stop(self):
+    pass
 
 def create_skill():
     return Aimar()
