ed3bea5e291b3d0a726d30424a9cecb429459c62
Kevin Kuo <kvkkuo@gmail.com>
2020-03-05 08:13:46 -0500

    modified movement for rclpy

--- aimar_move.py
+++ aimar_move.py
@@ -1,8 +1,46 @@
-import requests
+import rclpy
+from rclpy.node import Node
+from geometry_msgs import Twist
 
 ROBOT_URL = "localhost:5000"
+vel_pub = None
+
+
+class MovementPublisher(Node):
+
+    def __init__(self):
+        super().__init__('movement_publisher')
+        self.publisher_ = self.create_publisher(Twist, 'topic', 10)
+
+    def publish(self, msg):
+        self.publisher_.publish(msg)
 
 
 def move_simple(time, direction):
-    requests.get(f"{ROBOT_URL}/move?direction={direction}")
-    # after async delay stop
+    speed = 2.0
+    vel_msg = Twist()
+
+    if direction == 'forward':
+        vel_msg.linear.x = speed
+    elif direction == 'backward':
+        vel_msg.linear.x = -speed
+    elif direction == 'right':
+        vel_msg.angular.z = -speed
+    elif direction == 'left':
+        vel_msg.angular.z = speed
+    else:
+        return False
+    vel_pub.publish(vel_msg)
+
+
+def move_stop():
+    vel_msg = Twist()
+    vel_msg.linear.x = 0
+    vel_msg.angular.z = 0
+    vel_pub.publish(vel_msg)
+
+
+rclpy.init()
+vel_pub = MovementPublisher()
+# movement_publisher.destroy_node()
+# rclpy.shutdown()
